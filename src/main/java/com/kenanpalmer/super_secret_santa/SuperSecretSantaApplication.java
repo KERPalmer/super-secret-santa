f57809e39300ccba270d04ee5d6604d13ca151ad
kenan <kenan.palmer@icloud.com>
2025-01-30 12:00:56 +0000

    first commit - quick start template

---
+++ src/main/java/com/kenanpalmer/super_secret_santa/SuperSecretSantaApplication.java
@@ -0,0 +1,13 @@
+package com.kenanpalmer.super_secret_santa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSecretSantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSecretSantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenanpalmer/super_secret_santa/SuperSecretSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenanpalmer.super_secret_santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSecretSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
